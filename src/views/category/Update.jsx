7e556dfdd9c9876a7c6dc7ecfeee1a44d526ab3a
fozcini <45179499+fozcini@users.noreply.github.com>
2023-03-13 02:50:41 +0300

    Updated update.jsx
    rewrite

--- src/views/category/Update.jsx
+++ src/views/category/Update.jsx
@@ -1,35 +1,37 @@
-import { Spin } from "antd";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { Button, Input, Form } from "antd";
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 function Update() {
-  let navigate = useNavigate();
-  const [categoryEdit, setcategoryEdit] = useState({
-    name: "",
-    description: "",
-  });
-  const [loading, setloading] = useState(true);
-
   let { id } = useParams();
+  let navigate = useNavigate();
+  const [form] = Form.useForm();
 
   useEffect(() => {
     axios
       .get("https://northwind.vercel.app/api/categories/" + id)
       .then((res) => {
-        setcategoryEdit(res.data);
-        setloading(false);
+        console.log('asdas')
+        form.setFieldsValue({
+          name: res.data.name,
+          description: res.data.description,
+        });
       });
   }, []);
 
-  const handleEdit = (e) => {
-    setcategoryEdit({ ...categoryEdit, [e.target.name]: e.target.value });
-  };
-
-  const handleCategoryUpdate = (e) => {
-    e.preventDefault();
+  const updateCategory = () => {
     axios
-      .put("https://northwind.vercel.app/api/categories/" + id, categoryEdit)
+      .put("https://northwind.vercel.app/api/categories/" + id, {name: form.getFieldValue('name'), description: form.getFieldValue('description')})
       .then((res) => {
         navigate("/categories");
       });
@@ -37,30 +39,24 @@ function Update() {
 
   return (
     <>
-      <Spin tip="Loading..." spinning={loading}>
-        <h1>Update Category</h1>
-        <h1>Id: {categoryEdit.id} </h1>
-        <form onSubmit={handleCategoryUpdate}>
-          <label htmlFor="name">Name: </label>{" "}
-          <input
-            type="text"
-            name="name"
-            value={categoryEdit.name}
-            onChange={(e) => handleEdit(e)}
-          />{" "}
-          <br />
-          <label htmlFor="description">Description: </label>{" "}
-          <input
-            type="text"
-            name="description"
-            style={{ width: "60%" }}
-            value={categoryEdit.description}
-            onChange={(e) => handleEdit(e)}
-          />
-          <br />
-          <button type="submit">Update</button>
-        </form>
-      </Spin>
+      <Form
+        {...layout}
+        form={form}
+        name="control-hooks"
+        style={{ maxWidth: 600 }}
+      >
+        <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="Description" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit" onClick={updateCategory}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </>
   );
 }
